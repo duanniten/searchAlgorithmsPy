42347f93cbdb35c3f3eadeb62a0ae77c0a217fcc
duanniten <duanniten>
2024-07-07 18:58:21 -0300

    implemente Stack Frontier

---
+++ frontier/stackFrontier.py
@@ -0,0 +1,22 @@
+from ..node import Node
+
+class StackFrontier():
+    def __init__(self) -> None:
+        self.frontier = []
+    
+    def add(self, node: Node) -> None:
+        self.frontier.append(node)
+    
+    def empty(self) -> bool:
+        return len(self.frontier) == 0
+    
+    def containState(self, state: Node.state) -> bool:
+        return any(node.state == state for node in self.frontier)
+    
+    def remove(self) ->  Node:
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
